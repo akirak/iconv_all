e185842987cfaedf0291dbe2c002bced932b7d8a
Akira Komamura <akira.komamura@gmail.com>
2023-10-19 00:16:34 +0900

    dev: Add pre-commit-hooks

--- flake.nix
+++ flake.nix
@@ -1,36 +1,82 @@
 {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.pre-commit-hooks = {
+    url = "github:cachix/pre-commit-hooks.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
+  inputs.systems.url = "github:nix-systems/default";
+
   outputs = {
     self,
     nixpkgs,
+    systems,
     flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+    pre-commit-hooks,
+  } @ inputs: let
+    # Set the Erlang version
+    erlangVersion = "erlang_25";
+    # Set the Elixir version
+    elixirVersion = "elixir_1_14";
 
-        # Set the Erlang version
-        erlangVersion = "erlangR25";
-        # Set the Elixir version
-        elixirVersion = "elixir_1_14";
+    inherit (nixpkgs.lib) optional optionals;
 
-        erlang = pkgs.beam.interpreters.${erlangVersion};
-        elixir = pkgs.beam.packages.${erlangVersion}.${elixirVersion};
-        elixir-ls = pkgs.beam.packages.${erlangVersion}.elixir-ls;
-      in rec {
-        # TODO: Add your Elixir package
-        # packages = flake-utils.lib.flattenTree {
-        # } ;
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-        devShells.default = pkgs.mkShell {
+          erlang = pkgs.beam.interpreters.${erlangVersion};
+          beamPackages = pkgs.beam.packages.${erlangVersion};
+          elixir = beamPackages.${elixirVersion};
+        in
+          f (import nixpkgs {
+            inherit system;
+            overlays = [
+              (_: prev: {
+                inherit erlang elixir;
+                inherit (beamPackages) elixir-ls;
+              })
+            ];
+          })
+      );
+  in {
+    devShells = eachSystem (pkgs:
+      with pkgs; {
+        default = mkShell {
           buildInputs = [
             erlang
             elixir
             elixir-ls
           ];
+
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+
+          LANG = "C.UTF-8";
+          ERL_AFLAGS = "-kernel shell_history enabled";
         };
-      }
-    );
+      });
+
+    checks = eachSystem (pkgs: {
+      pre-commit-check = pre-commit-hooks.lib.${pkgs.system}.run {
+        src = ./.;
+        hooks = {
+          mix-format.enable = true;
+          # credo.enable = true;
+          dialyzer = {
+            enable = true;
+            stages = [
+              "push"
+            ];
+          };
+          mix-test = {
+            enable = true;
+            stages = [
+              "push"
+            ];
+          };
+        };
+      };
+    });
+  };
 }
